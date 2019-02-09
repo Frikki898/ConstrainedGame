864bbece2b15d770803d40336ea83304e07be97e
Frikki898 <frikki898@gmail.com>
2019-02-09 22:53:44 +0000

    ball is moving inside a box

---
+++ ConstrainedGame/Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+
+    void Start()
+    {
+        float x = Random.Range(40, 50f);
+        float y = Random.Range(40, 50f);
+        float z = Random.Range(40, 50f);
+        this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(speed * x, speed * y, speed * z);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        
+    }
+}
